8403e3cdba46d79dd4f924cbfced8f0c7c9e0044
Andy McBeath <mcbeath1@gmail.com>
2023-02-21 11:15:34 -0600

    try again'

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,4 @@ Rails.application.routes.draw do
   patch "/contacts/:id" => "contacts#update"
   get "bookings" => "bookings#index"
   post "/bookings" => "bookings#create"
-  resources :mailings, only: [:new, :create]
-  get "/mailings", to: "mailings#new", as: "contact"
-  get "mailings/sent"
 end
